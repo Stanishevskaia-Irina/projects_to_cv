bc2cc83adb3bfa7875842b875a5fbb08f291c562
Irishka <irishka66@gmail.com>
2017-11-08 20:24:51 +0300

    add working project

---
+++ my_best_adaptive/js/my_js.js
@@ -0,0 +1,106 @@
+$(document).ready(function() {
+	var sidebar = '.sidebar';
+	var navbarLinks = '.navbar > a';
+	var nav = '.nav';
+	var linkNav = '.link-nav';
+	var activeTab = 'active-tab';
+	var sidebarTitle = '.title';
+	var foldIcon = '.fold-icon';
+	var tags = '.sidebar .tags';
+	var navTabs = '.nav a';
+	var navChildren = $(nav).children();
+	var fixedClass = 'fixed';
+
+	var sidebarContainer = $(sidebar).children();
+	var navbar = $('.navbar');
+
+	$(navbarLinks).on('click', function() {
+		var linkClass = $(this).attr('class');
+		var navbarClass = navbar.attr('class');
+		/* check is user click on the active tab or no.
+			if click on active tab -- ignor action
+		*/
+		if ((linkClass.indexOf(activeTab) == -1) && (('.' + linkClass) != linkNav)) {
+			if (($(window).width() <= '767')) {
+				$(nav).hide();
+			}
+			$(sidebar).show();
+			sidebarContainer.hide();
+			sidebarContainer.filter('.' + linkClass).show();
+			setClassActiveTab(this);
+		}
+
+		if ((linkClass.indexOf(activeTab) == -1) && (('.' + linkClass) == linkNav)) {
+			hideSidebar();
+			$(navChildren).show();
+			$(nav).css('display', 'flex');
+			setClassActiveTab(this);
+		}
+
+		if (($(window).width() >= '768') && ($(window).width() < '1024')) {
+			$(tags).show();
+		}
+		/*if navbar are fixed on the top of window, move scrollbar on the top of body*/
+		if ((navbarClass.indexOf(fixedClass) != -1)) {
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+		}
+	});
+
+	
+	
+
+	// folding sidebar-area
+	$(sidebarTitle).click( function(){
+		if (($(window).width() <= '767')) {
+			hideSidebar();
+		}
+		if (($(window).width() >= '768') && ($(window).width() < '1024')) {
+			sidebarContainer.hide();
+			$(navbarLinks).removeClass(activeTab);
+			sidebarContainer.filter(tags).show();
+		}
+	});
+
+	$(foldIcon).on('click', hideSidebar);
+
+	$(foldIcon).on('click', hideNavTabs);
+
+	/*set class active-tab to tabs from nav*/
+	$(navTabs).click( function() {
+		$(navTabs).removeClass(activeTab);
+		$(this).toggleClass(activeTab);
+	})
+
+ /*fix navbar on the top of the page on scroll*/
+	$(window).scroll(function(){
+		var header = $('header');
+		var headerHeight = header.height();
+		var scroll = $(window).scrollTop();
+		if (scroll >= headerHeight) {
+			navbar.addClass('fixed');
+		}
+		else {
+			navbar.removeClass('fixed');
+		}
+	});
+
+	/*set class active-tab to tabs from navbar(not fron nav)*/
+	function setClassActiveTab(active) {
+		var currentActiveTab = active;
+		removeClassActiveTab();
+		$(active).toggleClass(activeTab);
+	}
+
+	function hideSidebar() {
+		$(sidebar).hide();
+		removeClassActiveTab();
+	}
+
+	function hideNavTabs() {
+		$(navChildren).hide();
+	}
+
+	function removeClassActiveTab() {
+		$(navbarLinks).removeClass(activeTab);
+	}
+});
